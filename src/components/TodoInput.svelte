1a19fd0c390a8d8a16394b64e6e8ee30c95b4390
BayuDC <dev.bayudc@gmail.com>
2022-02-16 22:30:07 +0700

    Add todo to the store

--- src/components/TodoInput.svelte
+++ src/components/TodoInput.svelte
@@ -1,11 +1,23 @@
 <script>
     import Card from '../shared/Card.svelte';
     import Button from '../shared/Button.svelte';
+    import Todo from '../lib/todo';
+    import { todos } from '../store';
+
+    let todoContet;
 </script>
 
 <Card>
-    <form on:submit|preventDefault>
-        <input type="text" placeholder="New Todo..." required />
+    <form
+        on:submit|preventDefault={() => {
+            todos.update(currentTodos => {
+                currentTodos.unshift(new Todo(todoContet));
+                return currentTodos;
+            });
+            todoContet = '';
+        }}
+    >
+        <input bind:value={todoContet} type="text" placeholder="New Todo..." required />
         <Button icon="add" />
     </form>
 </Card>
--- src/store.js
+++ src/store.js
@@ -1,4 +1,3 @@
 import { writable } from 'svelte/store';
-import Todo from './lib/todo';
 
-export const todos = writable([new Todo('Learn Svelte'), new Todo('Learn Django'), new Todo('Learn Go')]);
+export const todos = writable([]);
