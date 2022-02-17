84075eb9fc7efea4c7f8f3046d672b846ef082ac
BayuDC <dev.bayudc@gmail.com>
2022-02-17 08:37:21 +0700

    Store data in localStorage

--- src/store.js
+++ src/store.js
@@ -1,3 +1,7 @@
 import { writable } from 'svelte/store';
 
-export const todos = writable([]);
+export const todos = writable(JSON.parse(localStorage.getItem('todos')) || []);
+
+todos.subscribe(currentTodos => {
+    localStorage.setItem('todos', JSON.stringify(currentTodos));
+});
